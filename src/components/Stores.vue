6d3f3b1c82a423f7cbaabba545e60c53a68312c8
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-05-25 14:26:28 +0100

    added content to the store component

--- src/components/Stores.vue
+++ src/components/Stores.vue
@@ -1,4 +1,177 @@
 <template>
   <div class="row">
+    <div class="col-sm-12">
+      <div class="row text-center">
+
+          <div class="col-md-3 col-sm-6 hero-feature">
+              <div class="thumbnail">
+                  <img src="http://placehold.it/800x500" alt="">
+                  <div class="caption">
+                      <h3>Register!</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                  </div>
+              </div>
+          </div>
+
+          <div class="col-md-3 col-sm-6 hero-feature">
+              <div class="thumbnail">
+                  <img src="http://placehold.it/800x500" alt="">
+                  <div class="caption">
+                      <h3>Start your shop</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                  </div>
+              </div>
+          </div>
+
+          <div class="col-md-3 col-sm-6 hero-feature">
+              <div class="thumbnail">
+                  <img src="http://placehold.it/800x500" alt="">
+                  <div class="caption">
+                      <h3>Add your products</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                  </div>
+              </div>
+          </div>
+
+          <div class="col-md-3 col-sm-6 hero-feature">
+              <div class="thumbnail">
+                  <img src="http://placehold.it/800x500" alt="">
+                  <div class="caption">
+                      <h3>You're Ready</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div class="col-sm-12">
+        <div class="row text-center">
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/800x500" alt="">
+                    <div class="caption">
+                        <h3>Register!</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/800x500" alt="">
+                    <div class="caption">
+                        <h3>Start your shop</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/800x500" alt="">
+                    <div class="caption">
+                        <h3>Add your products</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/800x500" alt="">
+                    <div class="caption">
+                        <h3>You're Ready</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12">
+          <div class="row text-center">
+
+              <div class="col-md-3 col-sm-6 hero-feature">
+                  <div class="thumbnail">
+                      <img src="http://placehold.it/800x500" alt="">
+                      <div class="caption">
+                          <h3>Register!</h3>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                      </div>
+                  </div>
+              </div>
+
+              <div class="col-md-3 col-sm-6 hero-feature">
+                  <div class="thumbnail">
+                      <img src="http://placehold.it/800x500" alt="">
+                      <div class="caption">
+                          <h3>Start your shop</h3>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                      </div>
+                  </div>
+              </div>
+
+              <div class="col-md-3 col-sm-6 hero-feature">
+                  <div class="thumbnail">
+                      <img src="http://placehold.it/800x500" alt="">
+                      <div class="caption">
+                          <h3>Add your products</h3>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                      </div>
+                  </div>
+              </div>
+
+              <div class="col-md-3 col-sm-6 hero-feature">
+                  <div class="thumbnail">
+                      <img src="http://placehold.it/800x500" alt="">
+                      <div class="caption">
+                          <h3>You're Ready</h3>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="col-sm-12">
+            <div class="row text-center">
+
+                <div class="col-md-3 col-sm-6 hero-feature">
+                    <div class="thumbnail">
+                        <img src="http://placehold.it/800x500" alt="">
+                        <div class="caption">
+                            <h3>Register!</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 hero-feature">
+                    <div class="thumbnail">
+                        <img src="http://placehold.it/800x500" alt="">
+                        <div class="caption">
+                            <h3>Start your shop</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 hero-feature">
+                    <div class="thumbnail">
+                        <img src="http://placehold.it/800x500" alt="">
+                        <div class="caption">
+                            <h3>Add your products</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 hero-feature">
+                    <div class="thumbnail">
+                        <img src="http://placehold.it/800x500" alt="">
+                        <div class="caption">
+                            <h3>You're Ready</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
   </div>
 </template>
