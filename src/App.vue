bafe9c52a084a53bfe930498d83bd5df3a90e652
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-05-25 14:09:03 +0100

    modified the registered page to center content

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar top-bar"></span>
                     <span class="icon-bar middle-bar"></span>
@@ -21,7 +21,7 @@
                         <a v-link="'/login'">Login</a>
                     </li>
                 </ul>
-                <form class="navbar-form navbar-left">
+                <form class="navbar-form navbar-right">
                   <div class="form-group">
                     <button v-link="'/register'" class="btn btn-success">Register</a>
                   </div>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,21 +1,29 @@
 <template>
-  <form>
-    <div class="form-group">
-      <label for="firstname">FirstName:</label>
-      <input type="text" class="form-control" id="firstname" placeholder="First Name">
+  <div class="row">
+    <div class="col-sm-6 col-sm-push-3 col-xs-12">
+      <form>
+        <div class="form-group">
+          <label for="firstname">FirstName:</label>
+          <input type="text" class="form-control" id="firstname" placeholder="First Name">
+        </div>
+        <div class="form-group">
+          <label for="lastname">LastName:</label>
+          <input type="text" class="form-control" id="lastname" placeholder="Last Name">
+        </div>
+        <div class="form-group">
+          <label for="email">Email address</label>
+          <input type="email" class="form-control" id="email_field" placeholder="Email Address">
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" class="form-control" id="password_field" placeholder="Password">
+        </div>
+        <div class="form-group">
+          <label for="password">Confirm Password</label>
+          <input type="password" class="form-control" id="confirm_password_field" placeholder="Confirm Password">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
     </div>
-    <div class="form-group">
-      <label for="lastname">LastName:</label>
-      <input type="text" class="form-control" id="lastname" placeholder="Last Name">
-    </div>
-    <div class="form-group">
-      <label for="email">Email address</label>
-      <input type="email" class="form-control" id="email_field" placeholder="Email Address">
-    </div>
-    <div class="form-group">
-      <label for="password">Password</label>
-      <input type="password" class="form-control" id="password_field" placeholder="Password">
-    </div>
-    <button type="submit" class="btn btn-default">Submit</button>
-  </form>
+  </div>
 </template>
