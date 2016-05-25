0b61789690402bd911fdc67adb42112b7f1a040a
Olutola Oreofeoluwapo <oreofe.olutola@gmail.com>
2016-05-25 18:22:08 +0100

    firebasedb connection initialised in the firebase/index.js file

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,8 +1,13 @@
 // Initialize Firebase
- var config = {
-   apiKey: "AIzaSyDonFLkP43WNZF_m3raLR7PCscM6BNwL_M",
-   authDomain: "online-store-757b4.firebaseapp.com",
-   databaseURL: "https://online-store-757b4.firebaseio.com",
-   storageBucket: "online-store-757b4.appspot.com",
- };
- firebase.initializeApp(config);
+var config = {
+ apiKey: "AIzaSyDonFLkP43WNZF_m3raLR7PCscM6BNwL_M",
+ authDomain: "online-store-757b4.firebaseapp.com",
+ databaseURL: "https://online-store-757b4.firebaseio.com",
+ storageBucket: "online-store-757b4.appspot.com",
+};
+firebase.initializeApp(config);
+var firebaseDB = firebase.database();
+
+export default {
+  firebaseDB:firebaseDB,
+}
